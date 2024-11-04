2d79f81b5554243ba47ee9593bfdb7f5d3f7dd72
fitriana-24241052 <ftiriananingsih934@gamil.com>
2024-11-04 21:39:57 +0700

    Modul

---
+++ Modul 3/01-variabel/main.py
@@ -0,0 +1,25 @@
+#variabel adalah tempat menyimpan data
+
+#menaruh data/assigment data
+a=10
+x=5
+panjang=1000
+
+#memanggil variabel
+print("nilai a :", a)
+print("nilai x :", x)
+print("nilai panjang:",panjang)
+
+#penamaan variabel
+nilai_y=15 #dengan undescore
+juta10 =1000000000 #boleh dipakai
+nilaiangka=100 #model camel
+
+#mengubah isi variabel
+print("nilai a :",a) #nilai a awal=10
+a=7
+print("nilai a",a) #nilai diubah=7
+
+print("nilai x",x) #nilai x awal=5
+x=a
+print("x:",x) #nilai x diubah=7
